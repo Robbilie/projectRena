6638f2ea0dea4b3d74724fb6b2e49ad6c8caedab
Robert Schuh <github@eneticum.de>
2015-06-20 22:34:16 +0200

    more spaces 2

--- config/routes.php
+++ config/routes.php
@@ -274,40 +274,40 @@ $app->get('/contents/:id/', function($id) use ($app){
 */
 $app->get('/', function () use ($app)
 {
-				(new \ProjectRena\Controller\IndexController($app))->index();
+                (new \ProjectRena\Controller\IndexController($app))->index();
 });
 
 // Paste Page
 $app->get("/paste/", function () use ($app)
 {
-				(new \ProjectRena\Controller\PasteController($app))->pastePage();
+                (new \ProjectRena\Controller\PasteController($app))->pastePage();
 });
 $app->post("/paste/", function () use ($app)
 {
-				(new \ProjectRena\Controller\PasteController($app))->postPaste();
+                (new \ProjectRena\Controller\PasteController($app))->postPaste();
 });
 $app->get("/paste/:hash/", function ($hash) use ($app)
 {
-				(new \ProjectRena\Controller\PasteController($app))->showPaste($hash);
+                (new \ProjectRena\Controller\PasteController($app))->showPaste($hash);
 });
 
 // Login
 $app->get('/login/eve/', function () use ($app)
 {
-				(new \ProjectRena\Controller\LoginController($app))->loginEVE();
+                (new \ProjectRena\Controller\LoginController($app))->loginEVE();
 });
 
 // Logout
 $app->get('/logout/', function () use ($app)
 {
-				$sessionData = $_SESSION;
-				foreach($sessionData as $key => $val)
-				{
-								unset($_SESSION[$key]);
-				}
+                $sessionData = $_SESSION;
+                foreach($sessionData as $key => $val)
+                {
+                                unset($_SESSION[$key]);
+                }
 
-				$cookieName = $app->baseConfig->getConfig("name", "cookies");
-				$cookieSSL = $app->baseConfig->getConfig("ssl", "cookies");
-				$app->deleteCookie($cookieName, "/", $app->request->getHost(), $cookieSSL, true);
-				$app->redirect("/");
+                $cookieName = $app->baseConfig->getConfig("name", "cookies");
+                $cookieSSL = $app->baseConfig->getConfig("ssl", "cookies");
+                $app->deleteCookie($cookieName, "/", $app->request->getHost(), $cookieSSL, true);
+                $app->redirect("/");
 });
