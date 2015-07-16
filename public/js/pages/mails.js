ba0f0af4dd919e88fe251145ab462bad99767324
www-data <www-data@sheliak.rep.pm>
2015-07-16 13:16:26 +0200

    mails page gui test

---
+++ public/js/pages/mails.js
@@ -0,0 +1,8 @@
+mailsJS();
+function mailsJS () {
+	fadeOn($("#mailsConti"), 1);
+	/*ajax("/json/notifications/", function (r) {
+		console.log(r);
+		fadeOn($("#notificationsConti"), 1);
+	}, "json");*/
+}
