f18defc50b1f514cd05207095838663d5619d4c0
Robert Schuh <github@eneticum.de>
2015-07-24 03:08:59 +0200

    notification dump

--- public/js/pages/notifications.js
+++ public/js/pages/notifications.js
@@ -2,6 +2,12 @@ notificationsJS();
 function notificationsJS () {
 	ajax("/json/notifications/", function (r) {
 		console.log(r);
+		var list = $("#notificationsList");
+		var tmpl = $("#notificationTemplate").innerHTML;
+
+		for(var i = 0; i < r.length; i++)
+			list.appendChild(createElement(tmpl.format(["", JSON.stringify(r[i])])));
+
 		fadeOn($("#notificationsConti"), 1);
 	}, "json");
 }
